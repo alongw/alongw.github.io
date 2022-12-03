256c2f1aca09af4203ca1fc049172b7a188e386f
alongw <alongw@alongw.cn>
2022-12-04 02:42:59 +0800

    测试更新

--- bilibili/bilibili.js
+++ bilibili/bilibili.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         去除b站灰色以及广告拦截器插件弹窗
 // @namespace    https://i.alongw.cn/bilibili/bilibili.js/
-// @version      0.1
+// @version      0.2
 // @description  去除b站烦人的弹窗
 // @author       阿龙
 // @match        https://www.bilibili.com/
