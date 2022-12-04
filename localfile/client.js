99f8b2b150076d756d973f52fe0b0119ce03da78
alongw <alongw@alongw.cn>
2022-12-04 22:24:14 +0800

    更新链接

--- localfile/client.js
+++ localfile/client.js
@@ -1,1 +1,1 @@
-let downloadlink_server = 'https://i.alongw.cn/'
+let downloadlink_server = 'https://cloud.alongw.cn/api/v3/file/source/799/3.4.1.exe?sign=YUJM4czafZETfgMf23oVJRa_E_Bweh5BHeN_PQduuzM%3D%3A0'
